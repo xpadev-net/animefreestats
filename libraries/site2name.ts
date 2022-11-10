d71a788d4e8aee3483e0825cff42311a0ac4b160
xpa <xpadev@gmail.com>
2022-11-11 02:07:17 +0900

    fix: site name

--- libraries/site2name.ts
+++ libraries/site2name.ts
@@ -1,7 +1,7 @@
 const sites: { [key: number]: string } = {
-  1: "Abema",
+  1: "ABEMA",
   2: "GYAO!",
-  3: "ニコニコ動画",
+  3: "ニコニコ動画(Nアニメ)",
   4: "dアニメストア ニコニコ支店"
 };
 const site2name = (site: number): string => {
